2555f94ac09d3c26cc1cd7cfc5977ee130130bfb
Jfernandoff <153470746+Jfernandoff@users.noreply.github.com>
2024-01-24 04:45:39 +0000

    Wed, Jan 24, 2024, 1:45 AM -03:00

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
-
-#include <cs50.h>
 #include <stdio.h>
-
-string get_string(string prompt);
-int printf(string format, ...);
 
 int main(void)
 {
-  string name = get_string("What's your name? ");
-  printf("hello,%s\n", name);
+    printf("hello,%s\n");
 }
 
