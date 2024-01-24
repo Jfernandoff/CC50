9034aeb712e4042c4222785d2727ed9021ee9548
Jfernandoff <153470746+Jfernandoff@users.noreply.github.com>
2024-01-24 05:36:53 +0000

    Wed, Jan 24, 2024, 2:36 AM -03:00

--- addition2.c
+++ addition2.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
+    //Get numbers from user
     int x = get_int("x: ");
-
     int y = get_int("y: ");
 
+    //Divide x by y
     float z = x / y;
-
     printf("%f\n", z);
 }
