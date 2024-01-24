6cf5935f598e9a85b2df65a082ed42d647d22cab
Jfernandoff <153470746+Jfernandoff@users.noreply.github.com>
2024-01-24 06:21:41 +0000

    Wed, Jan 24, 2024, 3:21 AM -03:00

--- agree.c
+++ agree.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
-    
+    char c = get_char("Do you agree? ");
+    if (c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n')
+    {
+        printf("Not agreed.")
+    }
 }
