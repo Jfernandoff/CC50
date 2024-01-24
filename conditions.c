c21da3521908a73f3d774047fbb34483581c972a
Jfernandoff <153470746+Jfernandoff@users.noreply.github.com>
2024-01-24 06:02:18 +0000

    Wed, Jan 24, 2024, 3:02 AM -03:00

--- conditions.c
+++ conditions.c
@@ -18,6 +18,10 @@ int main(void)
     }
     else if (x > y)
     }
-    printf("x is greater than y\n");
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
     }
 }
